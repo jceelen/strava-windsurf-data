1b1d7a2aa2415c08d83b9c5a1c116da110c21603
jceelen <ceelen@gmail.com>
2018-06-23 15:47:10 +0200

    added location & countries and small improvements.

--- src/Strava_data.js
+++ src/Strava_data.js
@@ -1,16 +1,25 @@
 /**
  * A Google sheets script for retrieving windsurfing data from Strava
+ * retrieveData() is the main function
  */
 
+
+/**
+ * Gets the latest activities filters for windsurfing and writs it to the spreadsheet
+ * TODO: if sheet is empty retrieve all data => ensure header
+ * TODO: build in pagination and remove per_page=200 https://strava.github.io/api/#pagination
+ */
 function retrieveData() {
-  //TODO: if sheet is empty retrieve all data
+  
   var service = getService_();
+  // Check if the authorisation is in order
   if (service.hasAccess()) {
+    //Access the right sheet and get the date from the last entry
     var sheet = getStravaSheet();
     var unixTime = retrieveLastDate(sheet);
 
-    //TODO: build in pagination and remove per_page=200 https://strava.github.io/api/#pagination
-    //TODO: fix the parameter &after=' + unixTime
+    
+    // Get the activities since the last entry
     var url = 'https://www.strava.com/api/v3/athlete/activities?per_page=200&after=' + unixTime;
     if (DEBUG) Logger.log("URL: " + url);
     var response = UrlFetchApp.fetch(url, {
@@ -20,21 +29,25 @@ function retrieveData() {
     });
     var result = JSON.parse(response.getContentText());
 
-    //custom code starts here
+    // Check if there is any new data, if not, stop and log.
     if (result.length == 0) {
       Logger.log("No new data");
       return;
     }
 
+    // Filter only windsurf entries, format the data and put it in var data
     var data = convertData(result);
-
+    
+    // Check if there is any new data, if not, stop and log.
     if (data.length == 0) {
       Logger.log("No new data with windsurf activities");
       return;
     }
 
+    // Insert the new entries into the sheet
     insertData(sheet, data);
-
+  
+    // Authorize if needed
   } else {
     var authorizationUrl = service.getAuthorizationUrl();
     Logger.log('Open the following URL and re-run the script: %s',
@@ -42,6 +55,9 @@ function retrieveData() {
   }
 }
 
+/**
+ * Returns the date of the last entry in unixTime.
+ */
 function retrieveLastDate(sheet) {
   var lastRow = sheet.getLastRow();
   if (DEBUG) Logger.log("lastRow: " + lastRow);
@@ -60,17 +76,59 @@ function retrieveLastDate(sheet) {
    return unixTime;
 }
 
+/**
+ * Returns an array of windsurf items converted and formated to insert in the sheet
+ * 
+ * TODO: Fix descriptions as described here: You can use the Activities API to get activity 
+ * descriptions -  * https://developers.strava.com/playground/#/Activities/getActivityById. 
+ * If one exists, it will be there. Note that if you are getting activities in bulk such as 
+ * https://developers.strava.com/playground/#/Activities/getLoggedInAthleteActivities, you 
+ * only get a summary representation which does not include the description. For mout about 
+ * summary vs. detail representations, see https://strava.github.io/api/v3/activities/.
+ * 
+ */
 function convertData(result) {
   var data = [];
   for (var i = 0; i < result.length; i++) {
     if (result[i].type == "Windsurf") {
+      if (DEBUG) Logger.log("Activity: %s", result[i]);
+      
+      // Get the location address from the start_latlng
+      var city = "";
+      var country = "";
+      var start_lat = "";
+      var start_long = "";
+      
+      if (result[i].start_latlng !== null) {
+        start_lat = result[i].start_latlng[0];
+        start_long = result[i].start_latlng[1];
+        
+        if (DEBUG) Logger.log("Start Lat: %s", start_lat);
+        if (DEBUG) Logger.log("Start Long: %s", start_long);
+        address_components = getLocation(start_lat, start_long);
+        if (DEBUG) Logger.log("Address Components: %s", address_components);
+
+        city = extractFromAdress(address_components, "locality");
+        if (DEBUG) Logger.log("City: %s", city);
+
+        country = extractFromAdress(address_components, "country");
+        if (DEBUG) Logger.log("Country: %s", country);
+      } 
+
+      if (DEBUG) Logger.log("Description: %s", result[i].description);
+
+      // format all data before inserting it into the sheet
       var item = [result[i].start_date_local,
                   result[i].id,
                   result[i].name,
+                  result[i].description,
                   result[i].distance/1000,
-                  result[i].average_speed,
-                  result[i].max_speed,
-                  result[i].location_country];
+                  result[i].average_speed/0.27777777777778,
+                  result[i].max_speed/0.27777777777778,
+                  city,
+                  country,
+                  start_lat,
+                  start_long];
       data.push(item);
     }
   }
@@ -78,21 +136,34 @@ function convertData(result) {
   return data;
 }
 
+/**
+ * Returns the sheet defined in the config
+ */
 function getStravaSheet() {
   var spreadsheet = SpreadsheetApp.openById(SPREADSHEET_ID);
   var sheet = getOrCreateSheet(spreadsheet, SHEET_NAME);
   return sheet;
 }
 
+/**
+ * Creates a header if needed and inserts the data
+ * TODO: move header values to config
+ */
 function insertData(sheet, data) {
-  var header = ["Start Date", "Strava ID", "Name", "Distance", "Average Speed", "Max Speed", "Location"];
+  // Set header values
+  var header = ["Start Date", "Strava ID", "Name", "Description", "Distance", "Average Speed", "Max Speed", "City", "Country", "Latitude", "Longitude"];
+  // Make sure that there is a header in the sheet
   ensureHeader(header, sheet);
 
+  // Insert the data on the last row
   var lastRow = sheet.getLastRow();
-  var range = sheet.getRange(lastRow+1,1,data.length,7);
+  var range = sheet.getRange(lastRow+1,1,data.length,11);
   range.setValues(data);
 }
 
+/**
+ * Checks for a header and create one if not available
+ */
 function ensureHeader(header, sheet) {
   // Only add the header if sheet is empty
   if (sheet.getLastRow() == 0) {
@@ -106,6 +177,9 @@ function ensureHeader(header, sheet) {
   }
 }
 
+/**
+ * Gets or creates a sheet in the spreadsheet document with the correct name
+ */
 function getOrCreateSheet(spreadsheet, sheetName) {
   var sheet = spreadsheet.getSheetByName(sheetName);
 
@@ -117,6 +191,28 @@ function getOrCreateSheet(spreadsheet, sheetName) {
   return sheet;
 }
 
+/**
+ * Gets the address of the Lat Long location.
+ */
+function getLocation(lat, lng) {
+  var response = Maps.newGeocoder().reverseGeocode(lat, lng); 
+  for (var i = 0; i < response.results.length; i++) {
+    var result = response.results[i];
+    if (DEBUG) Logger.log('Result: %s', result.address_components);
+    return result.address_components;
+  }
+}
+
+/**
+ * Extracts any address component from the result of a google maps call
+ */
+function extractFromAdress(components, type){
+  for (var i=0; i<components.length; i++)
+      for (var j=0; j<components[i].types.length; j++)
+          if (components[i].types[j]==type) return components[i].long_name;
+  return "";
+}
+
 function test() {
   //var sheet = getStravaSheet();
   //var unixTime = retrieveLastDate(sheet);
@@ -125,3 +221,4 @@ function test() {
   test = test + "test";
   Logger.log(test);
 }
+
